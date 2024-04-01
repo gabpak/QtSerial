6b49b6f2546573c9d3483172a9aa8f3d37cf0b8a
Gabin PAQUES <106560023+gabpak@users.noreply.github.com>
2024-04-01 23:01:22 +0200

    Ajout dossier "ressources"

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setWindowIcon(QIcon(":/ece.ico")); // Chemin relatif au fichier de ressources
+    a.setWindowIcon(QIcon(":ressources/ece.ico")); // Chemin relatif au fichier de ressources
     MainWindow w;
     w.show();
     return a.exec();
